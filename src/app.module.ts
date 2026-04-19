da488d91b80bdc35b7b9eaeda5fd6d3f2276caa8
ThePauloRoberto <paulorantunes13@hotmail.com>
2026-04-19 00:40:02 -0300

    chore(app): Importando todos os módulos de http criados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmpresaModule } from './http/empresa/empresa.module';
+import { EnderecoModule } from './http/endereco/endereco.module';
+import { EntradaEstoqueModule } from './http/entrada-estoque/entrada-estoque.module';
+import { FornecedorModule } from './http/fornecedor/fornecedor.module';
+import { ItemEntradaModule } from './http/item-entrada/item-entrada.module';
+import { ItemVendaModule } from './http/item-venda/item-venda.module';
+import { LogModule } from './http/log/log.module';
+import { PagamentoModule } from './http/pagamento/pagamento.module';
+import { ProdutoModule } from './http/produto/produto.module';
+import { UsuarioModule } from './http/usuario/usuario.module';
+import { VendaModule } from './http/venda/venda.module';
+import { EnvModule } from './config';
+import { DatabaseModule } from './database/database.module';
+import { CategoriaModule } from './http/categoria/categoria.module';
+import { ClienteModule } from './http/cliente/cliente.module';
 
 @Module({
-  imports: [],
+  imports: [
+    EnvModule,
+    DatabaseModule,
+    CategoriaModule,
+    ClienteModule,
+    EmpresaModule,
+    EnderecoModule,
+    EntradaEstoqueModule,
+    FornecedorModule,
+    ItemEntradaModule,
+    ItemVendaModule,
+    LogModule,
+    PagamentoModule,
+    ProdutoModule,
+    UsuarioModule,
+    VendaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

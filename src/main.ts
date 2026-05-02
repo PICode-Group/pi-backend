83294b02750a0527123cce28479781193926e0da
ThePauloRoberto <paulorantunes13@hotmail.com>
2026-05-01 23:43:46 -0300

    feat(Docs): Configuração e Implementação inicial do Swagger na API e ZodJs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Itaprime')
+    .setVersion('1.0')
+    .setDescription('Documentação da API de gerenciamento da loja ITAPRIME')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
